5b44e67a2d5a2868d02d22837287fb14877a03ab
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-08-03 17:29:35 +0900

    単語ベクトル演算結果を画面のTextViewに表示する処理を追加。

--- app/src/main/java/com/example/analysiswordbyfasttextmobile/MainActivity.kt
+++ app/src/main/java/com/example/analysiswordbyfasttextmobile/MainActivity.kt
@@ -7,6 +7,7 @@ import android.support.design.widget.TextInputEditText
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.view.*
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -31,25 +32,36 @@ class MainActivity : AppCompatActivity() {
         val executeButton = findViewById<View>(R.id.executeButton)
         executeButton.setOnClickListener {
             val inputText1 = findViewById<View>(R.id.inputText1) as EditText
-            Log.d("checked", "${inputText1.text}")
-        }
+            val inputText2 = findViewById<View>(R.id.inputText2) as EditText
+            Log.d("input_text", "${inputText1.text}")
+            Log.d("input_text", "${inputText2.text}")
+            // 入力する単語情報をセット
+            val inputWord = linkedMapOf<String, String>()
+            inputWord.put("${inputText1.text}", "Negative")
+            inputWord.put("${inputText2.text}", "Positive")
+            // 素性ベクトルファイルの読み込み
+            val searchBr = BufferedReader(InputStreamReader(assets.open(Setting.model)))
+            val cosBr = BufferedReader(InputStreamReader(assets.open(Setting.model)))
 
-//        // 入力する単語情報をセット
-//        val inputWord = linkedMapOf<String, String>()
-//        inputWord.put("感情", "Negative")
-//        inputWord.put("人間", "Positive")
-//
-//        // 素性ベクトルファイルの読み込み
-//        val searchBr = BufferedReader(InputStreamReader(assets.open(Setting.model)))
-//        val cosBr = BufferedReader(InputStreamReader(assets.open(Setting.model)))
-//
-//        // 入力した単語の素性ベクトル情報をサーチ
-//        val vectorMap = Parser().searchInputWordVector(inputWord, searchBr)
-//        // 単語ベクトルの演算
-//        val (calculatedVector, joinedWord) = cal.calWordVector(inputWord, vectorMap)
-//        // 入力した単語と他の単語のコサイン類似度を計算
-//        val cosRank = cal.getWord2Vec(inputWord, joinedWord, calculatedVector, cosBr)
-//        // コサイン類似度の上位10位まで出力
-//        Sort().sortCosRank(cosRank)
+            // 入力した単語の素性ベクトル情報をサーチ
+            val vectorMap = Parser().searchInputWordVector(inputWord, searchBr)
+            // 単語ベクトルの演算
+            val (calculatedVector, joinedWord) = cal.calWordVector(inputWord, vectorMap)
+            // 入力した単語と他の単語のコサイン類似度を計算
+            val cosRank = cal.getWord2Vec(inputWord, joinedWord, calculatedVector, cosBr)
+            // コサイン類似度の上位10位まで出力
+//            Sort().sortCosRank(cosRank)
+            val sortedAnswer = cosRank.toList().sortedByDescending { it.second }.toMap()
+            val rank1 = findViewById<View>(R.id.textView1) as TextView
+            var i = 0
+            for (ans in sortedAnswer) {
+                i = i.plus(1)
+                println("${i}位：${ans}")
+                rank1.text = ans.toString()
+                if (i > 9) {
+                    break
+                }
+            }
+        }
     }
 }
